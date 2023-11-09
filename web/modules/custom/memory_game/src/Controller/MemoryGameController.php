e3c89a8573678bacb6ed80796a8d9e9209085fa0
Mike Lewis <michael.lewis2@vmlyr.com>
2023-11-09 16:36:26 -0500

    Controller logic to validate and then generate the appropriate response.

--- web/modules/custom/memory_game/src/Controller/MemoryGameController.php
+++ web/modules/custom/memory_game/src/Controller/MemoryGameController.php
@@ -44,26 +44,47 @@ class MemoryGameController extends ControllerBase {
    * Builds the response.
    */
   public function build(Request $request) {
-    // $rows = $request->query->get('rows');
-    // $columns = $request->query->get('columns');
-    $rows = 4;
-    $columns = 6;
-    $grid_generator = new GridGenerator();
-    $grid = $grid_generator->generateGrid($rows, $columns, TRUE);
-
+    // Initialize the status code and a response placeholder.
+    $status = 400;
     $response = [
       'meta' => [
-        'success' => TRUE,
-        'cardCount' => $grid_generator->getCardCount(),
-        'uniqueCardCount' => $grid_generator->getUniqueCardCount(),
-        'uniqueCards' => $grid_generator->getUniqueCards(),
+        'success' => FALSE,
+        'message' => 'Invalid request',
       ],
-      'data' => [
-        'cards' => $grid,
-      ],
+      'data' => [],
     ];
 
-    return new JsonResponse($response);
+    // Check the request for validity.
+    if (!$this->requestValidator->validateRequest($request)) {
+      // If the request is invalid, retrieve the error message to report
+      // back to the user.
+      $response['meta']['message'] = $this->requestValidator->validationError;
+    }
+    else {
+      // If the request is valid, set the status code, generate the grid, and
+      // update the output.
+      //
+      // Retrieve inputs from the request.
+      $rows = $request->query->get('rows');
+      $columns = $request->query->get('columns');
+
+      // Generate the grid.
+      $grid_generator = new GridGenerator();
+      $response['data']['cards'] = $grid_generator->generateGrid($rows, $columns);
+
+      // Update the response status and message.
+      $status = 200;
+      $response['meta']['success'] = TRUE;
+      unset($response['meta']['message']);
+
+      // Add metadata to the output.
+      $response['meta']['cardCount'] = $grid_generator->getCardCount();
+      $response['meta']['uniqueCardCount'] = $grid_generator->getUniqueCardCount();
+      $response['meta']['uniqueCards'] = $grid_generator->getUniqueCards();
+    }
+
+    // Return the response as JSON.
+    return new JsonResponse($response, $status);
   }
 
 }
